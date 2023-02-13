f745c0a73580e661753e5d87bd49c963f7eed2f5
Msluka <msluka.info@gmail.com>
2023-02-13 19:03:38 +0100

    Selecting multiple elements - Way 1

--- CollectionViewDemo/MVVM/Views/DataView.xaml.cs
+++ CollectionViewDemo/MVVM/Views/DataView.xaml.cs
@@ -11,11 +11,20 @@ public partial class DataView : ContentPage
 		BindingContext = new DataViewModel();
 	}
 
-  //  private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-  //  {
-		//var element = e.CurrentSelection;
-		//var name = (element.FirstOrDefault() as Models.Product).Name;
-		//Debug.WriteLine(name);		
+    private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var element_list = e.CurrentSelection;
+        var i = element_list.Count() - 1;
+        var name = (element_list[i] as Models.Product).Name;
+        Debug.WriteLine(name);
 
-  //  }
+    }
+
+    //  private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    //  {
+    //var element = e.CurrentSelection;
+    //var name = (element.FirstOrDefault() as Models.Product).Name;
+    //Debug.WriteLine(name);		
+
+    //  }
 }
