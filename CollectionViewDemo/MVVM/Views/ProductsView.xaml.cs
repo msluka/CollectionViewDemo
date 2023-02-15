0e6c3029aab0d1186923fc6f192a83fcbb37991f
Msluka <msluka.info@gmail.com>
2023-02-15 22:24:34 +0100

    Detecting Scrolling

--- CollectionViewDemo/MVVM/Views/ProductsView.xaml.cs
+++ CollectionViewDemo/MVVM/Views/ProductsView.xaml.cs
@@ -1,4 +1,5 @@
 using CollectionViewDemo.MVVM.ViewModels;
+using System.Diagnostics;
 
 namespace CollectionViewDemo.MVVM.Views;
 
@@ -9,4 +10,17 @@ public partial class ProductsView : ContentPage
 		InitializeComponent();
 		BindingContext = new ProductsViewModel();
 	}
+
+    private void CollectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+    {
+        Debug.Write("----------------------------------------");
+        Debug.WriteLine("HorizontalDelta: " + e.HorizontalDelta);
+        Debug.WriteLine("VerticalDelta: " + e.VerticalDelta);
+        Debug.WriteLine("HorizontalOffset: " + e.HorizontalOffset);
+        Debug.WriteLine("VerticalOffset: " + e.VerticalOffset);
+        Debug.WriteLine("FirstVisibleItemIndex: " + e.FirstVisibleItemIndex);
+        Debug.WriteLine("CenterItemIndex: " + e.CenterItemIndex);
+        Debug.WriteLine("LastVisibleItemIndex: " + e.LastVisibleItemIndex);
+        Debug.Write("----------------------------------------");
+    }
 }
